0f9208cdf4ddf0b7fd2c09cf288beb8abcf337df
noaha27 <67342736+noaha27@users.noreply.github.com>
2021-06-28 20:58:52 +0000

    script.js modifications

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP
+let buttonDiv;
+let happyButton;
+let sadButton;
+let trainButton
 
 // Global ML Variables
-//let featureExtractor;
+let featureExtractor;
+let classifier;
+let video;
+let happies;
+let sads;
+let isModelReady;
+let isTrainingComplete;
 
 function setup() {
 
+canvasDiv = createDiv();
+canvas = createCanvas(640, 480);
+canvas.parent(canvasDiv);
+textDiv = createDiv();
+textP = createP("Model loading, please wait...");
+textP.parent(textDiv);
+buildButtons();
+happies = 0;
+sads = 0;
+isModelReady = false;
+isTrainingComplete = false;
+video = createCapture(VIDEO, videoReady); 
 }
 
 function draw() {
@@ -15,7 +40,12 @@ function draw() {
 }
 
 function buildButtons() {
+  buttonDiv = createDiv();
+  happyButton = createButton("Happy");
+  happyButton.parent(buttonDiv);
+  happyButton.mousePressed(function() {
 
+  });
 }
 
 function videoReady() {
